b49ee6c44cb4ff1d7895097c44b46af8e3f827a5
lakmil <lakmil1999@gmail.com>
2021-03-26 18:49:50 +0530

    first commit

---
+++ src/components/Card.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../App.css';
+
+const Card = (props) =>  {
+    return(
+        <div className="card-grid">
+            <div className="card">
+                <img src={props.image} alt="Logo" />
+                <div className="card-body">
+                    <h5 className="card-title">{props.title}</h5>
+                    <p>Price:<span>{props.price}</span></p>
+                    <a href="/" className="btn btn-primary">Add to Cart</a>
+                </div>
+            </div>
+        </div>      
+    );
+}
+
+export default Card
---
+++ src/images/images.js
@@ -0,0 +1,8 @@
+import image1 from '../assets/load_more_img1.png';
+import image2 from '../assets/load_more_img2.png';
+import image3 from '../assets/load_more_img3@2x.png';
+import image4 from '../assets/our_plans2.png';
+
+const images = [image1,image2,image3,image4];
+
+export default images;
