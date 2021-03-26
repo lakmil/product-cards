12f342d92b3303f5d934a7771e7e56eaf8d22de6
lakmil <lakmil1999@gmail.com>
2021-03-26 18:50:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Card from './components/Card';
+import images from './images/images';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.js';
+
+class App extends Component {
+  render() {
+    const data = [
+      {"title": "Nike", "price": 300, "image": images[0]},
+      {"title": "Adidas", "price": 400, "image": images[1]},
+      {"title": "Puma", "price": 500, "image": images[2]},
+      {"title": "UCB", "price": 600, "image": images[3]},
+      {"title": "Bata", "price": 700, "image": images[1]},
+      {"title": "Kobler", "price": 300, "image": images[0]},
+    ];
+    return (
+      <div className="App container">
+        <div className="row">
+        {data.map((item,index)=> {
+           return <Card key={index} title={item.title} price = {item.price} image = {item.image} />;
+        })}
+
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
