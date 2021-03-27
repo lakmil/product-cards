96782cf2d1d6e227747b96dae7e23a0ce981acfa
lakmil <lakmil1999@gmail.com>
2021-03-27 23:34:05 +0530

    second commit

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import '../App.css';
 
 const Card = (props) =>  {
@@ -9,6 +10,7 @@ const Card = (props) =>  {
                 <div className="card-body">
                     <h5 className="card-title">{props.title}</h5>
                     <p>Price:<span>{props.price}</span></p>
+                    <p>Category:<span>{props.category}</span></p>
                     <a href="/" className="btn btn-primary">Add to Cart</a>
                 </div>
             </div>
